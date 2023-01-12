e6690f2b71a6a5cf6bce55370fe95598cfce208e
Christina Loiacono <loiaconochristina@gmail.com>
2023-01-12 18:46:35 -0500

    installing scss and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
+
+// Components
+import Navbar from './components/Navbar';
+import Menu from './components/Menu';
+
 function App() {
+  const API = process.env.REACT_APP_API_URL;
+
+  const [pizzaData, setPizzaData] = useState([]);
+
+  useEffect(() => {
+    try {
+      fetch(`${API}/pizza`)
+        .then((res) => res.json())
+        .then(data => {
+          console.log(data)
+          setPizzaData(data);
+        });   
+    } catch (err){
+      console.log(err)
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route path="/menu" element={<Menu pizzaData={pizzaData} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
